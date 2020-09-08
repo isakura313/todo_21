f15cf506502658fbf193e0b12d7ee945dc04dc8b
Paul <isakura313@gmail.com>
2020-09-08 13:40:38 +0300

    asdasd

---
+++ ItemDeal.js
@@ -0,0 +1,7 @@
+export default class ItemDeal{
+    constructor(text, color){
+        this.text = text;
+        this.color = color;
+        this.now = new Date;
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const concat = require("gulp-concat");
 const rename = require("gulp-rename");
+const cssmin = require("gulp-cssmin");
 
 const paths = {
     styles: {
@@ -9,32 +10,32 @@ const paths = {
     }
   };
    
-
-   
   /*
    * Define our tasks using plain functions
    */
   function styles() {
     return gulp.src(paths.styles.src)
         .pipe(concat(paths.styles.dest))
+
         .pipe(rename(function (path) {
           path.extname = ".css";
         }))
+
+        .pipe(cssmin())
       .pipe(gulp.dest(paths.styles.dest));
   }
    
-
    
   function watch() {
     gulp.watch(paths.styles.src, styles);
   }
    
 
-  var build = gulp.series(gulp.parallel(styles));
+  // var build = gulp.series(gulp.parallel(styles));
    
 
   exports.styles = styles;
   exports.watch = watch;
-  exports.build = build;
+  // exports.build = build;
 
-  exports.default = build;
+  // exports.default = build;
